034b85d18d4462ec26c832db82dc7104e407ce2e
Steven Pyper <steven.pyper@gmail.com>
2022-12-14 13:01:16 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,13 @@
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(cors())
 
-app.get('/', (req,res) => {
-    console.log(__dirname)
-    res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
-})
 
-app.get('/css', (req,res) => {
-    res.status(200).sendFile(path.join(__dirname, '../public/styles.css'))
-})
 
-app.listen(4000, console.log("Server running on 4000."))
+
+
+app.listen(4000, console.log('Server running on 4000'))
