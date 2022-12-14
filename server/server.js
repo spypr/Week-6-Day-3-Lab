74e2691896029921a6196fd6e5fab573c75a8a3e
Steven Pyper <steven.pyper@gmail.com>
2022-12-14 14:57:21 -0700

    path added again

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
+// const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
+
 
 app.get('/', (req,res) => {
-    res.status(200).sendFile(path.join(__dirname))
+    // console.log(__dirname)
+    res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
 })
-
-
+app.get('/css', (req,res) => {
+    res.status(200).sendFile(path.join(__dirname, '../public/styles.css'))
+})
 
 
 
