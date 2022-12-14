4a65819a517175e169c2e836e132d9ffce4b4ae8
Steven Pyper <steven.pyper@gmail.com>
2022-12-14 14:47:29 -0700

    path added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.get('/', (req,res) => {
+    res.status(200).sendFile(path.join(__dirname))
+})
 
 
 
